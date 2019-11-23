adbe8693d567b2d5e6a5c68e4cb034d099e42189
Virsaviya <virsaviya@Stardust-21.local>
2019-11-23 16:52:14 -0500

    add <AppBar/>

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
-import {
-  BrowserRouter as Router
-  // Router,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import theme from "./assets/theme";
-// import { GlobalWrapper } from "./styles";
-// import AppBar from "components/AppBar";
+import AppBar from "components/AppBar";
 import Routes from "./Routes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* <Router history={history}> */}
-      <div>
-        <Router>
-          {/* <AppBar /> */}
+      <div style={{ marginTop: "64px" }}>
+        <BrowserRouter>
+          <AppBar />
           <Routes />
-        </Router>
+        </BrowserRouter>
       </div>
     </ThemeProvider>
   );
