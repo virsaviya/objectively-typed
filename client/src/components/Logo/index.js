0b84f15371bbcb2668a9af5b087c5238984cbfbc
Virsaviya <virsaviya@Stardust-21.local>
2020-02-08 10:45:08 -0800

    update color gradient

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -4,32 +4,71 @@ import React from 'react';
 // https://www.quackit.com/css/css_color_codes.cfm
 
 const VIEW = 500;
-const NUMBER_OF_SQUARES = 40; // play with me
+const NUMBER_OF_SQUARES = 54; // play with me
+const STROKE_COLORS = {
+  // play with me
+  outerColor: {
+    red: 160,
+    green: 207,
+    blue: 255,
+  },
+  innerColor: {
+    red: 212,
+    green: 232,
+    blue: 233,
+  },
+};
+const FILL_COLORS = {
+  // play with me
+  outerColor: {
+    red: 44,
+    green: 160,
+    blue: 249,
+  },
+  innerColor: {
+    red: 136,
+    green: 191,
+    blue: 251,
+  },
+};
 
 const Logo = () => {
-  let angle = -3; // play with me
-  let size = 200; // play with me
-  let red = 50; // play with me
-  let green = 150; // play with me
-  let blue = 200; // play with me
+  const getColor = (start, end, frequency) => {
+    const difference = end - start;
+    const interval = difference / NUMBER_OF_SQUARES;
+    const currentValue = start + Math.round(interval * frequency);
+    return currentValue;
+  };
 
+  const getRgb = (colors, frequency) => {
+    const { outerColor, innerColor } = colors;
+    const red = getColor(outerColor.red, innerColor.red, frequency);
+    const green = getColor(outerColor.green, innerColor.green, frequency);
+    const blue = getColor(outerColor.blue, innerColor.blue, frequency);
+    return `rgb(${red}, ${green}, ${blue})`;
+  };
+
+  let angle = -3; // play with me
+  let size = 300; // play with me
   const emptyArray = [...Array(NUMBER_OF_SQUARES)];
-  const squares = emptyArray.map(() => {
+  const squares = emptyArray.map((_, idx) => {
+    const topLeft = (VIEW - size) / 2;
+    const fill = getRgb(FILL_COLORS, idx);
+    const stroke = getRgb(STROKE_COLORS, idx);
+    const key = `rect_${idx}`;
+
     angle += 3; // play with me
     size -= 5; // play with me
-    red += 3; // play with me
-    green += 2; // play with me
-    blue += 6; // play with me
-    const color = `rgb(${red}, ${green}, ${blue})`;
-    const topLeft = (VIEW - size) / 2;
+
     return (
       <rect
+        key={key}
         x={topLeft}
         y={topLeft}
         height={size}
         width={size}
-        fill={color}
-        stroke="midnightblue" // play with me
+        fill={fill}
+        stroke={stroke}
         rx="15"
         transform={`rotate(${angle} ${topLeft + size / 2} ${topLeft +
           size / 2} )`}
