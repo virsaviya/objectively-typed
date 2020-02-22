08fc45a2f63bfa24c7ca2dcc57d504c6bead69fa
Virsaviya <virsaviya@Stardust-21.local>
2020-02-22 14:17:20 -0800

    setup-apollo-client-with-example

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import ApolloClient, { gql } from 'apollo-boost';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,17 +12,37 @@ import theme from 'assets/theme';
 import Routes from 'Routes';
 import GlobalWrapper from './styles';
 
+// example endpoint, until we get our own API up
+// @TODO https://trello.com/c/gfYXoxCE/68-setup-graphql-apollo-server-side
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalWrapper>
-        <BrowserRouter>
-          <AppBar />
-          <Routes />
-        </BrowserRouter>
-      </GlobalWrapper>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalWrapper>
+          <BrowserRouter>
+            <AppBar />
+            <Routes />
+          </BrowserRouter>
+        </GlobalWrapper>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import FauxContent from 'components/FauxContent';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+// example fetch, until we get our own API up
+// @TODO https://trello.com/c/gfYXoxCE/68-setup-graphql-apollo-server-side
+const EXCHANGE_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+const ExchangeRates = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+};
 
 const Home = () => {
   return (
     <div>
-      <FauxContent title="Home" />
+      <ExchangeRates />
     </div>
   );
 };
